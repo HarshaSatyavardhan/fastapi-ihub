0b8d039e1004465ae6ecf564ee878fc2acbcd87a
specialized boy <41695878+HarshaSatyavardhan@users.noreply.github.com>
2022-02-07 01:27:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -403,7 +403,7 @@ async def predict_two(background_tasks: BackgroundTasks,solute):
     background_tasks.add_task(predictions_two,solute)
 
 if __name__ == "__main__":
-    uvicorn.run(app)
+  uvicorn.run("server.api:app", host="0.0.0.0", port=8000, reload=True)
 
 
 
